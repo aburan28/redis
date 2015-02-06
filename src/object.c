3353891c2cddcbc4628ee5646b747de6faebf72c
Adam Buran <aburan28@gmail.com>
2015-02-06 18:27:53 -0500

    added cast REDIS_ISET to decrRefCount

--- src/object.c
+++ src/object.c
@@ -233,6 +233,8 @@ void decrRefCount(robj *o) {
         case REDIS_SET: freeSetObject(o); break;
         case REDIS_ZSET: freeZsetObject(o); break;
         case REDIS_HASH: freeHashObject(o); break;
+        case REDIS_ISET: freeIsetObject(o); break;
+
         default: redisPanic("Unknown object type"); break;
         }
         zfree(o);
